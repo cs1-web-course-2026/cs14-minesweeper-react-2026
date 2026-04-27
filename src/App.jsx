ba796085808305dfcf8872260b845e7e152069ec
Alina <alinakvitochka888@gmail.com>
2026-04-27 13:25:21 +0200

    fix: restore removed import

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Game from './pages/Game'
 import MockGame from './pages/MockGame'
+import MockGameCopy from './pages/MockGameCopy'
 import KvitkaAlinaGame from "./pages/KvitkaAlina";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="game" element={<Game />} />
         <Route path="mock-game" element={<MockGame />} />
+        <Route path="mock-game-copy" element={<MockGameCopy />} />
         <Route path="kvitka-alina" element={<KvitkaAlinaGame />} />
       </Route>
     </Routes>
