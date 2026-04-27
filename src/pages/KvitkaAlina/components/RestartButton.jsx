f81e60d3f761768523f4aaf25135e9e908ab7ea2
Alina <alinakvitochka888@gmail.com>
2026-04-27 13:29:55 +0200

    fix: add aria-label to restart button

--- src/pages/KvitkaAlina/components/RestartButton.jsx
+++ src/pages/KvitkaAlina/components/RestartButton.jsx
@@ -2,8 +2,13 @@ import styles from "../styles/RestartButton.module.css";
 
 export default function RestartButton({ onRestart, face }) {
   return (
-    <button className={styles.button} onClick={onRestart} type="button">
-      {face}
-    </button>
+    <button
+  className={styles.button}
+  onClick={onRestart}
+  type="button"
+  aria-label="Restart game"
+>
+  {face}
+</button>
   );
 }
