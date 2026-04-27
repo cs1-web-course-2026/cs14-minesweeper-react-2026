246f58eef8c1a801e88808bb4f9080680c3fdfcd
alina-kvitka <kvitka.alina@student.karazin.ua>
2026-04-27 13:57:48 +0200

    Remove MockGameCopy route from App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Game from './pages/Game'
 import MockGame from './pages/MockGame'
-import MockGameCopy from './pages/MockGameCopy'
 import KvitkaAlinaGame from "./pages/KvitkaAlina";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="game" element={<Game />} />
         <Route path="mock-game" element={<MockGame />} />
-        <Route path="mock-game-copy" element={<MockGameCopy />} />
         <Route path="kvitka-alina" element={<KvitkaAlinaGame />} />
       </Route>
     </Routes>
