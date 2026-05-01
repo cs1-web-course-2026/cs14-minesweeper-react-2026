38aa3e9c8fe82fb4bf2e36cd6b92b0e3a9f588f4
yasinskaanastasiia <yasinska.anastastasiia@student.karazin.ua>
2026-05-01 11:31:12 +0300

    fix: clean App route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import YasinskaAnastasiiaGame from './pages/YasinskaAnastasiia';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
 import Game from './pages/Game'
-import MockGame from './pages/MockGame';
+import MockGame from './pages/MockGame'
+import YasinskaAnastasiiaGame from './pages/YasinskaAnastasiia'
 
 function App() {
   return (
     <Routes>
-  <Route path="/" element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="game" element={<Game />} />
-    <Route path="mock-game" element={<MockGame />} />
-    <Route path="yasinska-anastasiia" element={<YasinskaAnastasiiaGame />} />
-  </Route>
-</Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="game" element={<Game />} />
+        <Route path="mock-game" element={<MockGame />} />
+        <Route path="yasinska-anastasiia" element={<YasinskaAnastasiiaGame />} />
+      </Route>
+    </Routes>
   )
 }
 
