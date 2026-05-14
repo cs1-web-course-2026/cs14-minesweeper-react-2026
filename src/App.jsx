833f8dd4652ccffb9cc64b76f6b745e0ca2b2d00
Peresechanskyi Hryhorii <peresechanskyi.hryhorii@student.karazin.ua>
2026-05-14 15:41:08 +0300

    Fix route path for PeresechanskyiGame

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="game" element={<Game />} />
         <Route path="mock-game" element={<MockGame />} />
-        <Route path="/PeresechanskyiHryhorii" element={<PeresechanskyiGame />} />
+        <Route path="peresechanskyi-hryhorii" element={<PeresechanskyiGame />} />
       </Route>
     </Routes>
   )
